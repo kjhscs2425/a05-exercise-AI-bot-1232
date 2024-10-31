ea4b52100b72578d3d7697a2ed715f0085e082ce
AI-bot-1232 <nries2026@kehillahstudent.org>
2024-10-31 11:30:24 -0700

    I have finished the fever assigment

--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 any lower temperature, it should return `False`.
 """
 def check_fever(temperature):
-  #YOUR CODE HERE
-  return
+  return temperature>=100.4
 
-# Get temperature from user and convert to float
-temp = ...
-if check_fever(temp):
-  print("You have a fever.")
+# this is where I finished the code and where I can mess around
+temperature = 100
+if check_fever(temperature):
+  print ("sadly you have a fever")
 else:
-  print("You don't have a fever.")
+  print("congrats you don't have a fever")
+
+  
